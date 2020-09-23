50a976efbb2ca41202c2997cc7e3f66fdfa4644c
Stas <bloodofabyss@yandex.ru>
2020-09-23 19:16:20 +0300

    Вынес все сообщения в отдельный файл

--- utils/messages.js
+++ utils/messages.js
@@ -3,25 +3,30 @@ const messages = {
     authIsSuccess: 'Авторизация прошла успешно',
     notAuthorised: 'Необходима авторизация',
     wrongEmailOrPassword: 'Неправильная почта или пароль',
-
   },
-  card: {
-    isNotValid: 'Ошибка валидации данных карточки',
-    isDeleted: 'Карточка удалена',
-    idIsNotFound: 'Нет карточки с таким id',
-    idIsNotValid: 'id карточки невалиден',
+  article: {
+    isNotValid: 'Ошибка валидации данных статьи',
+    isDeleted: 'Статья удалена',
+    idIsNotFound: 'Нет статьи с таким id',
+    idIsNotValid: 'id статьи невалиден',
+    isCreated: 'Статья успешно добавлена',
   },
   registration: {
     emailIsNotUnique: 'Данный email уже зарегестрирован',
   },
   user: {
-    idIsNotValid: 'id пользователя невалиден',
+    passwordIsNotValid:
+      'Длинна пароля менее 8 символов, либо пароль не валиден',
     idIsNotFound: 'Нет пользователя с таким id',
-    passwordIsNotValid: 'Длинна пароля менее 8 символов, либо пароль не валиден',
   },
   validation: {
     urlIsNotValid: 'URL невалиден',
     notFound: 'Запрашиваемый ресурс не найден',
+  },
+  schemas: {
+    isEmpty: 'Поле не может быть пустым',
+    wrongUrl: 'Неправильный формат ссылки',
+    emailIsNotValid: 'Email невалиден',
   },
 };
 
