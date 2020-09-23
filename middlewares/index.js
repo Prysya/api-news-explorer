d048744eb11985b50ee445087513e4c27776704a
Stas <bloodofabyss@yandex.ru>
2020-09-23 19:13:51 +0300

    Подключил мидлвары в index

--- middlewares/index.js
+++ middlewares/index.js
@@ -4,7 +4,9 @@ const bodyParser = require('./body-parser');
 const cookieParser = require('./cookie-parser');
 const helmet = require('./helmet');
 const rateLimiter = require('./rate-limiter');
+const cors = require('./cors');
 
+router.use(cors);
 router.use(bodyParser);
 router.use(cookieParser);
 router.use(helmet);
