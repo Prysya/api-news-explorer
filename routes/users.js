45caee1e0d28a8f8868b287860f5695aac70eb42
Stas <bloodofabyss@yandex.ru>
2020-09-23 19:15:07 +0300

    Добавил роуты для пользователей

--- routes/users.js
+++ routes/users.js
@@ -1,43 +1,12 @@
 const router = require('express').Router();
-const { celebrate } = require('celebrate');
 const {
-  getUsers,
-  getUser,
-  updateUserData,
-  updateUserAvatar,
+  getUserInfo,
 } = require('../controllers/users');
-const {
-  verifyUserId,
-} = require('../middlewares/object-id');
-const {
-  userInfoSchema,
-  userAvatarSchema,
-  objectIdSchema,
-} = require('../joi-schemas/index');
+
 const auth = require('../middlewares/auth');
 
 router.use(auth);
 
-router.get('/', getUsers);
-router.get(
-  '/:id',
-  celebrate({ params: objectIdSchema }),
-  verifyUserId,
-  getUser,
-);
-
-router.patch(
-  '/me',
-  celebrate({
-    params: objectIdSchema,
-    body: userInfoSchema,
-  }),
-  updateUserData,
-);
-router.patch(
-  '/me/avatar',
-  celebrate({ params: objectIdSchema, body: userAvatarSchema }),
-  updateUserAvatar,
-);
+router.get('/me', getUserInfo);
 
 module.exports = router;
