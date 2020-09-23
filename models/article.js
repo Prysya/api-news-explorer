ccbef2eb5487c1ee190817c8613d6f4f7792abb8
Stas <bloodofabyss@yandex.ru>
2020-09-23 19:06:15 +0300

    Создал схему для статей

--- models/article.js
+++ models/article.js
@@ -1,12 +1,55 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const messages = require('../utils/messages');
 
 const cardSchema = new mongoose.Schema({
-  name: {
+  keyword: {
     type: String,
-    required: true,
+    required: [true, 'Длинна ключевого слова должна быть от 2 до 30 символов'],
     minlength: 2,
     maxlength: 30,
+    validate: {
+      validator(text) {
+        return !validator.isEmpty(text, { ignore_whitespace: true });
+      },
+      message: messages.schemas.isEmpty,
+    },
+  },
+  title: {
+    type: String,
+    required: [true, 'Длинна заголовка должна быть от 2 до 30 символов'],
+    minlength: 2,
+    maxlength: 30,
+    validate: {
+      validator(text) {
+        return !validator.isEmpty(text, { ignore_whitespace: true });
+      },
+      message: messages.schemas.isEmpty,
+    },
+  },
+  text: {
+    type: String,
+    required: [true, 'Длинна статьи должна быть от 2 символов'],
+    minlength: 2,
+    maxlength: 30,
+    validate: {
+      validator(text) {
+        return !validator.isEmpty(text, { ignore_whitespace: true });
+      },
+      message: messages.schemas.isEmpty,
+    },
+  },
+  date: {
+    type: String,
+    required: [true, 'Длинна даты должна быть от 2 символов'],
+    minlength: 2,
+    maxlength: 30,
+    validate: {
+      validator(text) {
+        return !validator.isEmpty(text, { ignore_whitespace: true });
+      },
+      message: messages.schemas.isEmpty,
+    },
   },
   link: {
     type: String,
@@ -14,24 +57,25 @@ const cardSchema = new mongoose.Schema({
       validator(link) {
         return validator.isURL(link);
       },
-      message: 'Неправильный формат ссылки',
+      message: messages.schemas.wrongUrl,
     },
-    required: true,
+    required: [true, 'Ссылка на статью обязательна'],
+  },
+  image: {
+    type: String,
+    validate: {
+      validator(link) {
+        return validator.isURL(link);
+      },
+      message: messages.schemas.wrongUrl,
+    },
+    required: [true, 'Ссылка на изображение обязательна'],
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
     required: true,
-  },
-  likes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      default: [],
-    },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now,
+    select: false,
   },
 });
 
