f923c5770deee0e3ebda24a47da5ed1d2912423d
Stas <bloodofabyss@yandex.ru>
2020-09-23 19:09:15 +0300

    Написал функции для работы с пользователями

--- controllers/users.js
+++ controllers/users.js
@@ -1,29 +1,15 @@
 const bcrypt = require('bcryptjs');
 const escape = require('escape-html');
-const { createToken } = require('../utils/token');
+const { createToken, messages } = require('../utils');
 const User = require('../models/user');
-const schemaValidator = require('../utils/passwordValidator');
-const {
-  BadRequestError,
-  NotFoundError,
-} = require('../errors/index');
-const messages = require('../utils/messages');
+const { BadRequestError, NotFoundError } = require('../errors');
 
-module.exports.getUsers = async (req, res, next) => {
+module.exports.getUserInfo = async (req, res, next) => {
   try {
-    const users = await User.find({});
-    res.send({ data: users });
-  } catch (err) {
-    next(err);
-  }
-};
-
-module.exports.getUser = async ({ params: { id } }, res, next) => {
-  try {
-    const user = await User.findById(id).orFail(
+    const user = await User.findById(req.user._id).orFail(
       () => new NotFoundError(messages.user.idIsNotFound),
     );
-    res.send({ data: user });
+    res.send({ data: { name: user.name, email: user.email } });
   } catch (err) {
     next(err);
   }
@@ -31,67 +17,21 @@ module.exports.getUser = async ({ params: { id } }, res, next) => {
 
 module.exports.createUser = async (req, res, next) => {
   try {
-    const {
-      name, about, avatar, email, password,
-    } = req.body;
-
-    if (
-      !password
-      || password.length < 8
-      || !schemaValidator.validate(password)
-    ) {
-      throw new BadRequestError(messages.user.passwordIsNotValid);
-    }
+    const { name, email, password } = req.body;
 
     const hash = await bcrypt.hash(password, 10);
     const user = await User.create({
       name: escape(name),
-      about: escape(about),
-      avatar,
       email,
       password: hash,
     });
 
     res.send({
       data: {
-        email: user.email,
         name: user.name,
-        about: user.about,
-        avatar: user.avatar,
+        email: user.email,
       },
     });
-  } catch (err) {
-    next(new BadRequestError(err.message));
-  }
-};
-
-module.exports.updateUserData = async (req, res, next) => {
-  try {
-    const { name, about } = req.body;
-
-    const user = await User.findByIdAndUpdate(
-      req.user._id,
-      { name: escape(name), about: escape(about) },
-      { new: true, runValidators: true },
-    ).orFail(() => new NotFoundError(messages.user.idIsNotFound));
-
-    res.send({ data: user });
-  } catch (err) {
-    next(new BadRequestError(err.message));
-  }
-};
-
-module.exports.updateUserAvatar = async (req, res, next) => {
-  try {
-    const { avatar } = req.body;
-
-    const user = await User.findByIdAndUpdate(
-      req.user._id,
-      { avatar },
-      { new: true, runValidators: true },
-    ).orFail(() => new NotFoundError(messages.user.idIsNotFound));
-
-    res.send({ data: user });
   } catch (err) {
     next(new BadRequestError(err.message));
   }
@@ -105,14 +45,12 @@ module.exports.login = async (req, res, next) => {
 
     const token = await createToken(user);
 
-    await res
-      .cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-        sameSite: true,
-      });
+    await res.cookie('jwt', token, {
+      maxAge: 3600000 * 24 * 7,
+      httpOnly: true,
+    });
 
-    res.status(200).send({ message: messages.auth.authIsSuccess });
+    await res.status(200).send({ message: messages.auth.authIsSuccess });
   } catch (err) {
     next(err);
   }
