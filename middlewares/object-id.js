278d44eedec6325fea1fbade1d2d28efde9ad00e
Stas <bloodofabyss@yandex.ru>
2020-09-23 19:12:10 +0300

    Добавил функцию для проверки валидного id

--- middlewares/object-id.js
+++ middlewares/object-id.js
@@ -2,10 +2,6 @@ const { ObjectId } = require('mongodb');
 const messages = require('../utils/messages');
 const { BadRequestError } = require('../errors/index');
 
-module.exports.verifyCardId = (req, res, next) => (ObjectId.isValid(req.params.id)
+module.exports.verifyArticleId = (req, res, next) => (ObjectId.isValid(req.params.articleId)
   ? next()
-  : next(new BadRequestError(messages.card.idIsNotValid)));
-
-module.exports.verifyUserId = (req, res, next) => (ObjectId.isValid(req.params.id)
-  ? next()
-  : next(new BadRequestError(messages.user.idIsNotValid)));
+  : next(new BadRequestError(messages.article.idIsNotValid)));
