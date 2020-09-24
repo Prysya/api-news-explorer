8f7e4c2a02a8f19704f842a610091a2bf1b247eb
Stas <bloodofabyss@yandex.ru>
2020-09-24 20:12:54 +0300

    Исправил импорт сообщений об ошибках

--- middlewares/errors-handler.js
+++ middlewares/errors-handler.js
@@ -1,6 +1,8 @@
 module.exports.errorsHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
+  console.log(err)
+
   res.status(statusCode).send({
     message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
