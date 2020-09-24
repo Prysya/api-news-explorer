9f3f67b5a27cc68c598c5201d04f887391da47d7
Stas <bloodofabyss@yandex.ru>
2020-09-24 20:13:36 +0300

    Исправил импорт сообщений об ошибках

--- routes/errors.js
+++ routes/errors.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { NotFoundError } = require('../errors/index');
-const messages = require('../utils/messages');
+const { messages } = require('../utils');
 
 router.all('*', (req, res, next) => {
   next(new NotFoundError(messages.validation.notFound));
