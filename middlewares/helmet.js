2de4b4ce7a5cee84cc72362ca9fbff1efc3202bd
Stas <bloodofabyss@yandex.ru>
2020-09-25 23:32:57 +0300

    Удалил отдельный роутер для helmet, перенес подключение в файл index

--- middlewares/helmet.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const helmet = require('helmet');
-
-router.use(helmet());
-
-module.exports = router;
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 
+const helmet = require('helmet');
 const bodyParser = require('./body-parser');
 const cookieParser = require('./cookie-parser');
-const helmet = require('./helmet');
 const rateLimiter = require('./rate-limiter');
 const cors = require('./cors');
 
 router.use(cors);
 router.use(bodyParser);
 router.use(cookieParser);
-router.use(helmet);
+router.use(helmet());
 router.use(rateLimiter);
 
 module.exports = router;
