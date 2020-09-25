24fd8859149f3d48a2ac3e195c9f14d556ee5a82
Stas <bloodofabyss@yandex.ru>
2020-09-25 23:48:42 +0300

    Удалил ненужные правила из .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,5 @@ module.exports = {
   },
   rules: {
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
-    'no-console': 'off',
-    'no-unused-vars': ['error', { args: 'none' }],
   },
 };
