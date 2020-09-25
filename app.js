92510dcd397f8bcdcbb4dce18220235882c95ae8
Stas <bloodofabyss@yandex.ru>
2020-09-25 23:40:48 +0300

    Вынес все мидлвары, логгер, и централизованный обработчик ошибок в корневой файл

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
 const router = require('./routes/index');
 const { dbOptions, dbUrl, port } = require('./utils');
+
+const middlewaresRouter = require('./middlewares');
+const { errorsHandler } = require('./middlewares/errors-handler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
 mongoose.connect(dbUrl, dbOptions);
 
+app.use(middlewaresRouter);
+
+app.use(requestLogger);
+
 app.use(router);
+
+router.use(errorLogger);
+
+router.use(errors());
+
+router.use(errorsHandler);
 
 app.listen(port, () => console.log(`Слушаю порт ${port}`));
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,15 @@
-const { errors } = require('celebrate');
 const router = require('express').Router();
 
-const middlewaresRouter = require('../middlewares');
-const { errorsHandler } = require('../middlewares/errors-handler');
 const register = require('./registration');
 const authorization = require('./authorization');
 const users = require('./users');
 const articles = require('./articles');
 const error = require('./errors');
-const { requestLogger, errorLogger } = require('../middlewares/logger');
-
-router.use(middlewaresRouter);
-
-router.use(requestLogger);
 
 router.use('/signin', authorization);
 router.use('/signup', register);
 router.use('/articles', articles);
 router.use('/users', users);
 router.use('*', error);
-
-router.use(errorLogger);
-
-router.use(errors());
-
-router.use(errorsHandler);
 
 module.exports = router;
