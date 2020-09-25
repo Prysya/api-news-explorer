fd595269854ad83a738575117fa552418520b353
Stas <bloodofabyss@yandex.ru>
2020-09-25 23:37:47 +0300

    Добавил импорт 403 ошибки в index.js

--- errors/index.js
+++ errors/index.js
@@ -1,5 +1,8 @@
 const BadRequestError = require('./bad-request-error');
 const NotFoundError = require('./not-found-error');
 const UnauthorizedError = require('./unauthorized-error');
+const ForbiddenError = require('./forbidden-error');
 
-module.exports = { BadRequestError, NotFoundError, UnauthorizedError };
+module.exports = {
+  BadRequestError, NotFoundError, UnauthorizedError, ForbiddenError,
+};
