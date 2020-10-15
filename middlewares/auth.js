08b08f4c72be4660b1b837c3491802c07d18e39e
Stas <bloodofabyss@yandex.ru>
2020-10-15 14:54:38 +0300

    Добавил проверку на Safari

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,19 @@ const { UnauthorizedError } = require('../errors/index');
 const { messages } = require('../utils/messages');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  let token;
+
+  if (req.headers['user-agent'].includes('Safari')) {
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+      throw new UnauthorizedError(messages.auth.notAuthorised);
+    }
+
+    token = authorization.replace('Bearer ', '');
+  } else {
+    token = req.cookies.jwt;
+  }
 
   if (!token) {
     throw new UnauthorizedError(messages.auth.notAuthorised);
